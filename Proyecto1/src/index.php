9a6d16d264d207c82cbc8f73448a2fb979beae45
Fleiren <hiirelu@gmail.com>
2025-09-24 20:12:28 +0200

    practicando dividir una página en partes

---
+++ Proyecto1/src/index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Índice de ejercicios</title>
+</head>
+<body>
+    <h1>Ejercicios de DWES</h1>
+    <ol>
+        <li><a href="src/ejercicios/bucle.php">Creación de un bucle en php</a></li>
+    </ol>
+</body>
+</html>
